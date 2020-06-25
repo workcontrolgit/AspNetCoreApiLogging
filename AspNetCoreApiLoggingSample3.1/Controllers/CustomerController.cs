c8a9f6031938c8b01d7d2ff86e30259525d4cf88
fuji.nguyen <fuji.nguyen@workcontrol.com>
2020-06-25 17:13:55 -0400

    Add swagger

--- AspNetCoreApiLoggingSample3.1/Controllers/CustomerController.cs
+++ AspNetCoreApiLoggingSample3.1/Controllers/CustomerController.cs
@@ -9,7 +9,7 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace AspNetCoreApiLoggingSample.Controllers
 {
-    [Route("api/[controller]")]
+    [Route("api/v1/[controller]")]
     [ApiController]
     public class CustomerController : ControllerBase
     {
